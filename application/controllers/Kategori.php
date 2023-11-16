1f6e5939394aafb16755f07db8269d068256004c
Maung90 <levriwidodo@gmail.com>
2023-11-16 12:09:46 +0800

    home user

--- application/controllers/Kategori.php
+++ application/controllers/Kategori.php
@@ -7,6 +7,11 @@
 class Kategori extends CI_Controller
 {
 	
+	function __construct()
+	{ 
+		parent::__construct();
+		$this->load->model('MKategori');
+	}
 	public function index()
 	{
 		$data['title'] = "POLITEKNIK NEGERI BALI";
@@ -16,6 +21,35 @@ class Kategori extends CI_Controller
 		$this->load->view('admin/kategori/index');
 		$this->load->view('partials/copyright');
 		$this->load->view('partials/footer');
+	}
+
+	public function insertData()
+	{
+		$this->MKategori->insert();
+	}
+
+	public function deleteData($id)
+	{
+		$this->MKategori->delete($id);
+	}
+
+
+
+	public function proses_session()
+	{
+
+		if (!$this->session->userdata('session')) {
+			$temp[] = $this->input->post();
+			$this->session->set_userdata('session',$temp);
+		}else{
+			$tempLama =  $this->session->userdata('session');
+			$tempLama[] = $this->input->post(); 
+
+			$this->session->set_userdata('session',$tempLama);
+		}
+
+		echo count($this->session->userdata('session'));
+
 	}
 
 }
--- application/views/user/index.php
+++ application/views/user/index.php
@@ -1,101 +1,167 @@
-    <nav class="navbar navbar-expand bg-primary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Info</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">  
-                <i class="fas fa-suitcase text-white"></i>
-                <span class="badge bg-danger rounded-pill badge-notifications">5</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+ <?php 
+ $this->session->sess_destroy();
+ ?>
+ <nav class="navbar navbar-expand bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Info</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">  
+            <i class="fas fa-suitcase text-white"></i>
+            <span class="badge bg-danger rounded-pill badge-notifications" id="notif">0</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
-
-    <div class="container">
-      <div class="row mt-5">
-        <div class="col-4">
-          <label for="select2Icons" class="form-label">Kategori</label>
-          <select id="select2Icons" class="select2-icons form-select">
-            <optgroup label="Peralatan">
-              <option value="" data-icon="ti ti-brand-bootstrap" selected>Proyektor</option>
-              <option value="" data-icon="ti ti-brand-codepen">Kabel</option>
-              <option value="" data-icon="ti ti-brand-php">HDMI</option>
-              <option value="" data-icon="ti ti-brand-css3">CSS3</option>
-              <option value="" data-icon="ti ti-brand-html5">HTML5</option>
-            </optgroup>
-          </select>
-        </div>
-        <div class="col-8 d-flex justify-content-end">
-          <div class="input-group input-group-merge mt-4 " style="width:60%;">
-            <span class="input-group-text" id="basic-addon-search31"><i class="ti ti-search"></i></span>
-            <input
-            type="text"
-            class="form-control"
-            placeholder="Search..."
-            aria-label="Search..."
-            aria-describedby="basic-addon-search31" />
-          </div>
-        </div>
-      </div>
-      <div class="row mt-3">
-        <div class="col-6 col-lg-3 my-2">
-          <div class="card  h-100 w-100">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <img
-              class="img-fluid d-flex mx-0 my-2 rounded"
-              src="<?= base_url('assets/img/elements/1.jpg'); ?>"
-              alt="Card image cap"/>
-              <button class="btn btn-sm btn-outline-primary">Card link</button>
-            </div>
-          </div>
-        </div> 
-        <div class="col-6 col-lg-3  my-2">
-          <div class="card  h-100 w-100">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <img
-              class="img-fluid d-flex mx-0 my-2 rounded"
-              src="<?= base_url('assets/img/elements/2.jpg'); ?>"
-              alt="Card image cap" />
-              <button class="btn btn-sm btn-outline-primary">Card link</button>
-            </div>
-          </div>
-        </div> 
-        <div class="col-6 col-lg-3  my-2">
-          <div class="card h-100 w-100">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <img
-              class="img-fluid d-flex mx-0 my-2 rounded"
-              src="<?= base_url('assets/img/elements/3.jpg'); ?>"
-              alt="Card image cap" />
-              <button class="btn btn-sm btn-outline-primary">Card link</button>
-            </div>
-          </div>
-        </div> 
-        <div class="col-6 col-lg-3  my-2">
-          <div class="card  h-100 w-100">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <img
-              class="img-fluid d-flex mx-0 my-2 rounded"
-              src="<?= base_url('assets/img/elements/4.jpg'); ?>"
-              alt="Card image cap" />
-              <button class="btn btn-sm btn-outline-primary">Card link</button>
-            </div>
-          </div>
-        </div> 
+<div class="container">
+  <div class="row mt-5"> 
+    <div class="col-12 d-flex justify-content-end">
+      <div class="input-group input-group-merge mt-4 " style="width:60%;">
+        <span class="input-group-text" id="basic-addon-search31"><i class="ti ti-search"></i></span>
+        <input
+        type="text"
+        class="form-control"
+        placeholder="Search..."
+        aria-label="Search..."
+        aria-describedby="basic-addon-search31" />
       </div>
     </div>
   </div>
+  <div class="row mt-3 ">
+    <?php  
+    $result = $this->db->get('tbkategori')->result(); 
+        // $id = 1;
+    foreach ($result as $data)  :
+     ?>
+     <div class="col-6 col-lg-3  my-2">
+      <div class="card  h-100 w-100">
+        <div class="card-body">
+          <h5 class="card-title"> <?= $data->nama_kategori;  ?> </h5>
+          <img
+          class="img-fluid d-flex mx-0 my-2 rounded"
+          src="<?= base_url('assets/img/elements/4.jpg'); ?>"
+          alt="Card image cap" />
+          <button type="button" class="btn btn-sm btn-outline-primary" id="id-<?=$data->id_kategori?>" data-bs-toggle="modal" data-bs-target="#modalCenter-<?=$data->id_kategori?>">
+            Pinjam Barang
+          </button> 
+        </div>
+      </div>
+      <form id="peminjamanForm">
+        <div class="modal fade" id="modalCenter-<?=$data->id_kategori?>" tabindex="-1" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="modalCenterTitle"><?=$data->nama_kategori;?></h5>
+                <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <div class="row">
+                  <div class="col-12">
+                    <label for="jumlah" class="form-label">Jumlah</label>
+                    <input
+                    type="number"
+                    id="jumlah-<?=$data->id_kategori?>"
+                    name="jumlah"
+                    class="form-control"
+                    placeholder="Masukan Jumlah" min="0"/>
+                    <input type="hidden" id='idKategori-<?=$data->id_kategori?>' name="idKategori" value="<?=$data->id_kategori?>">
+                  </div>
+                  <div class="col-12 text-secondary" style="font-size:0.8rem">
+                    *Stok Tersedia : 8
+                  </div>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-label-secondary" data-bs-dismiss="modal">
+                  Close
+                </button>
+                <button type="button" class="btn btn-primary" id="simpan-<?=$data->id_kategori?>">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div> 
+
+    <?php 
+  endforeach;
+  ?>
+</div>
+</div>
+
+
+<section id="adv-footer"> 
+  <footer class="footer bg-light">
+    <div class="container-fluid container-p-x pt-3 pb-2">
+      <div class="row">
+        <div class="col-12 col-sm-6 col-md-3 mb-4 mb-sm-0">
+          <h4 class="fw-bolder mb-3">
+            <a
+            href="#"
+            target="_blank"
+            class="footer-text"
+            >Politeknik Negeri Bali
+          </a>
+        </h4>
+        <div class="col-12 col-sm-6 col-md-3 mb-4 mb-md-0">
+          <h5>Contact</h5>
+          <ul class="list-unstyled">
+            <li><a href="javascript:void(0)" class="footer-link d-block pb-2">Admin I </a></li> 
+          </div> 
+          <p class="pt-4">
+            <script>
+              document.write(new Date().getFullYear());
+            </script>
+            © Team2One
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</section>
+
+<!-- </div> -->
+
+<script>
+
+  $(document).ready(function(){
+
+    var angkaNotif = 0;
+    $('[id^=simpan]').on('click',function(){
+      var id = $(this).attr('id').split('-')[1]; 
+      var data = { 
+        idKategori : $('#idKategori-'+id).val(),
+        jumlah : $('#jumlah-'+id).val()
+      };
+      $.ajax({
+        url : '<?= base_url('Kategori/proses_session');?>',
+        type : 'POST',
+        data : data,
+        success : function (response){ 
+          $('#notif').text(response);
+          $('#jumlah-'+id).val('0');
+        },
+        error : function (xhr, status, error) {
+          console.error('gagal mengubah sesi' + error);
+        }
+
+      });
+    });
+
+  });
+
+</script>
