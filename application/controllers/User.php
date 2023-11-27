870d8bff8675b1f8cfc3eb6b39ae9b66a3dafe92
WhosWibawa <wibawaib6904@gmail.com>
2023-11-27 12:15:18 +0800

    status

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -13,4 +13,15 @@ class User extends CI_Controller
 		$this->load->view('user/index'); 
 		$this->load->view('partials/footer');
 	}
+
+	public function status(){
+		$this->load->view('partials/side');
+		$this->load->view('partials/nav');
+		$this->load->view('user/status');
+		$this->load->view('partials/head');
+		$this->load->view('partials/footer');
+		$this->load->view('partials/copyright');
+
+		
+	}
 }
