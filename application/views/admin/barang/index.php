d49a2dcc41cc602365bc905c94e056d8cc30d1b4
usgaoh <agusputraa9999@gmail.com>
2023-12-27 11:04:28 +0800

    .

--- application/views/admin/barang/index.php
+++ application/views/admin/barang/index.php
@@ -11,7 +11,6 @@
             echo $this->session->userdata('pesan');
         }
     ?>
-        <?php //echo form_open_multipart('Barang/simpan', array('method' => 'post')); ?>
         <form action="<?= base_url('Barang/simpan')?>" method="POST" id="form" enctype="multipart/form-data">
         <div class="row mb-3">
             <label class="col-sm-2 col-form-label" for="basic-default-name">Kode Barang</label>
@@ -38,7 +37,6 @@
             <option selected disabled>Pilih</option>
             <option value="Tersedia">Tersedia</option>
             <option value="Tidak Tersedia">Tidak Tersedia</option>
-            <option value="Dipinjam">Dipinjam</option>
             </select>
             </div>
         </div>
@@ -117,9 +115,9 @@
           <button onclick="edit('<?= $value->kode_barang; ?>')" type="button" class="btn btn-sm btn-primary">
             <label class="ti ti-edit "></label>
           </button>
-        	<a href="<?= base_url("barang/hapus/".$value->kode_barang);?>" class="btn btn-sm btn-danger">
+        	<button onclick="hapus('<?= $value->kode_barang;?>')" type="button" class="btn btn-sm btn-danger">
           <label class="ti ti-trash"></label>
-          </a>
+          </button>
 
         </td>
       </tr>
@@ -129,8 +127,8 @@
     ?>
   </tbody>
 </table>
-        </div>
     </div>
+  </div>
 </div>
 <script>
     $(document).ready(function(){
@@ -140,7 +138,7 @@
 
 <script>
 
-function hapus(id) {
+  function hapus(id) {
 			Swal.fire({
 				icon: "question",
 				title: "Apakah yakin ingin menghapus data ini?",
